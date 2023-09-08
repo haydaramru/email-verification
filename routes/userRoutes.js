4d1a267eff6cb81c5ae1a1bd1addda180344a347
Haydar <haydaramrurevanda@mail.ugm.ac.id>
2023-09-08 22:34:00 +0700

    added login and email-verify routes

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -9,3 +9,11 @@ const router = Express.Router()
 // signup endpoint
 // passing the middleware function to the signup
 router.post("/signup", userAuth.saveUser, signup);
+
+// login route
+router.post("/login", login)
+
+// email verification route
+router.get("/verify-email/:id/:token", verifyEmail);
+
+module.exports = router
